04546953a12a8b7a3d54a0f4cae683b09b6b0a02
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-25 13:41:51 -0700

    add CRUD endpoints

--- api/views.py
+++ api/views.py
@@ -26,3 +26,35 @@ def productList(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def productDetail(request, pk):
+    products = Product.objects.get(id=pk)
+    serializer = ProductSerializer(products, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def productCreate(request):
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def productUpdate(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def productDelete(request, pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+
+    return Response("Deleted")
